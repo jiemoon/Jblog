0502ed265a9c117bebc4e7dc781b1480126a790b
jiemoon <jiemoon2010@gmail.com>
2017-04-02 15:01:45 +0800

    调整文章页面样式

--- resources/views/web/articles/show.blade.php
+++ resources/views/web/articles/show.blade.php
@@ -1,10 +1,17 @@
 @extends('layouts.web')
 
 @section('content')
-<h1>
-    {{ $article->title }}
-</h1>
-<div class="contetn">
-    {!! Markdown::convertToHtml($article->content) !!}
+<div class="post">
+    <article class="post-block">
+        <h1 class="post-title">
+            {{ $article->title }}
+        </h1>
+        <div class="post-time">
+            {{ generate_article_date($article->publish_at) }}
+        </div>
+        <div class="post-content">
+            {!! Markdown::convertToHtml($article->content) !!}
+        </div>
+    </article>
 </div>
 @endsection
